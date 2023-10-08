ec9042354a96a38c174082534a67a83177e060c4
x0lotl <danil200319771@gmail.com>
2023-10-08 16:13:55 +0300

    feat: add fake ui

--- src/popup/Popup.vue
+++ src/popup/Popup.vue
@@ -1,20 +1,97 @@
 <script setup lang="ts">
-import { ref } from 'vue'
+import { onMounted, ref } from 'vue'
 
 const crx = ref('create-chrome-ext')
+
+const counter = ref(parseInt(localStorage.counter) ?? 0);
+const disabled = ref(false);
+
+onMounted(() => {
+  setInterval(() => {
+    if (!disabled.value) {
+      counter.value += (Math.random() * 10);
+      localStorage.counter = counter.value;
+    }
+  }, Math.random() * 2000);
+});
 </script>
 
 <template>
   <main>
-    <h3>Popup Page!</h3>
-
-    <h6>v 0.0.0</h6>
-
-    <a href="https://www.npmjs.com/package/create-chrome-ext" target="_blank">Power by {{ crx }}</a>
+    <img class="logo" src="https://upload.wikimedia.org/wikipedia/commons/e/eb/Adblock_logo.png?20170914173748">
+    <h2>ADBlock by Inc-Dev</h2>
+    <div class="container">
+      <div class="counter" :class="{ 'couter-disabled': disabled }">
+        <div>
+          <h3 :class="{ 'text-disabled': disabled }">
+            {{ counter.toFixed(0) }}<br>
+          </h3>
+          Ads blocked
+        </div>
+      </div>
+    </div>
+    <div class="container mt-2">
+      <button @click.stop="disabled = !disabled" class="button" :class="{ 'button-disabled': disabled }">
+        {{ disabled ? 'Enable' : 'Disable' }}
+      </button>
+    </div>
   </main>
 </template>
 
 <style scoped>
+.button {
+  background-color: #ff0000;
+  border: 1px solid #ff0000;
+  color: #ffffff;
+  padding: 10px;
+  border-radius: 5px;
+  font-size: 1rem;
+  font-weight: 600;
+  text-transform: uppercase;
+  transition: all 0.2s ease-in-out;
+  cursor: not-allowed;
+}
+
+.button-disabled {
+  background-color: #57625d;
+  border: 1px solid #3c403e;
+  color: #ffffff;
+  cursor: pointer;
+}
+
+.mt-2 {
+  margin-top: 30px;
+}
+
+.container {
+  display: flex;
+  justify-content: center;
+}
+
+.text-disabled {
+  color: gray;
+}
+
+.counter {
+  border: 20px solid green;
+  width: 100px;
+  height: 100px;
+  border-radius: 100%;
+  padding: 15px;
+}
+
+.couter-disabled {
+  border: 20px solid rgb(58, 56, 56);
+  color: gray !important;
+}
+
+.logo {
+  width: 100px;
+  height: 100px;
+  margin: 0 auto;
+  display: block;
+}
+
 main {
   text-align: center;
   padding: 1em;
@@ -24,10 +101,10 @@ main {
 h3 {
   color: #42b983;
   text-transform: uppercase;
-  font-size: 1.5rem;
-  font-weight: 200;
+  font-size: 2.5rem;
+  font-weight: 800;
   line-height: 1.2rem;
-  margin: 2rem auto;
+  margin-bottom: 5px;
 }
 
 h6 {
