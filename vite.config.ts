320e5e184c0e33c5bca2e9de6d765d19afc0d53b
Artem Tarasenko <shabashab.04@gmail.com>
2023-10-08 15:22:06 +0300

    init commit

---
+++ vite.config.ts
@@ -0,0 +1,34 @@
+import { defineConfig } from 'vite'
+import { crx } from '@crxjs/vite-plugin'
+import vue from '@vitejs/plugin-vue'
+import zipPack from 'vite-plugin-zip-pack'
+import manifest from './src/manifest'
+
+// https://vitejs.dev/config/
+export default defineConfig(({ mode }) => {
+  const production = mode === 'production'
+
+  return {
+    build: {
+      emptyOutDir: true,
+      outDir: 'build',
+      rollupOptions: {
+        output: {
+          chunkFileNames: 'assets/chunk-[hash].js',
+        },
+      },
+    },
+    plugins: [
+      crx({ manifest }),
+      vue(),
+      zipPack({
+        outDir: `package`,
+        inDir: 'build',
+        // @ts-ignore
+        outFileName: `${manifest.short_name ?? manifest.name.replaceAll(' ', '-')}-extension-v${
+          manifest.version
+        }.zip`,
+      }),
+    ],
+  }
+})
